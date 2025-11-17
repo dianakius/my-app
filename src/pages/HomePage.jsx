c3ec0a321fc6bf9a1b9e65180a866c7d43d5801a
dianakius <eingidiana@gmail.com>
2025-11-17 20:24:19 +0200

    feat: use the component in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
+import FerrisWheel from "./FerrisWheel";
+
+
 export default function HomePage() {
   return (
     <div className="p-6">
       <h1 className="text-3xl font-bold text-center">Welcome to the Cat World</h1>
+      <FerrisWheel />
     </div>
+  
   );
 }
