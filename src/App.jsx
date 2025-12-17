7e9e6336117f7b71a797d4e14bf6990d22ca46e9
dianakius <eingidiana@gmail.com>
2025-12-17 17:30:13 +0200

    feat: import Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,27 @@ import { Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CatListPage from "./pages/CatListPage";
 import CatDetailsPage from "./pages/CatDetailsPage";
-import './App.css'
+import Footer from "./pages/Footer";
+import "./App.css";
 
-
-  function App() {
+function App() {
   return (
     <div className="min-h-screen bg-gray-100 text-gray-800">
       <nav className="flex justify-center gap-6 p-4 bg-white shadow">
-        <Link to="/" className="hover:underline">Home</Link>
-        <Link to="/cats" className="hover:underline">Cats</Link>
+        <Link to="/" className="hover:underline">
+          Home
+        </Link>
+        <Link to="/cats" className="hover:underline">
+          Cats
+        </Link>
       </nav>
 
-    
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cats" element={<CatListPage />} />
         <Route path="/cats/:id" element={<CatDetailsPage />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
