b43931f2cfed4c5b873f28e1fdc33e6d9da94ceb
dianakius <eingidiana@gmail.com>
2025-12-18 11:20:20 +0200

    refactor: remove the FerrisWheel import from HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,9 @@
-import FerrisWheel from "./FerrisWheel";
-
-
 export default function HomePage() {
   return (
     <div className="p-6">
-      <h1 className="text-3xl font-bold text-center">Welcome to the Cat World</h1>
-      <FerrisWheel />
+      <h1 className="text-3xl font-bold text-center">
+        Welcome to the Cat World
+      </h1>
     </div>
-  
   );
 }
