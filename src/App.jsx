350e7df25289b25d5fa351a6652d858d9c0ffae1
dianakius <eingidiana@gmail.com>
2025-12-22 21:51:51 +0200

    fix: duplicated and nestes structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import HomePage from "./pages/HomePage";
 import CatListPage from "./pages/CatListPage";
 import CatDetailsPage from "./pages/CatDetailsPage";
 import Navbar from "./components/Navbar";
-import "./App.css";
 import Footer from "./components/Footer";
 import "./App.css";
 
@@ -18,21 +17,6 @@ function App() {
           <Route path="/cats/:id" element={<CatDetailsPage />} />
         </Routes>
       </main>
-    <div className="min-h-screen bg-gray-100 text-gray-800">
-      <nav className="flex justify-center gap-6 p-4 bg-white shadow">
-        <Link to="/" className="hover:underline">
-          Home
-        </Link>
-        <Link to="/cats" className="hover:underline">
-          Cats
-        </Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/cats" element={<CatListPage />} />
-        <Route path="/cats/:id" element={<CatDetailsPage />} />
-      </Routes>
       <Footer />
     </div>
   );
