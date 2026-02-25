835da613be1d93cb6a0efe2d05315f2af8180bcb
dianakius <eingidiana@gmail.com>
2026-02-25 14:04:37 +0200

    feat: create a new page components:Contact

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+export default function Contact() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold text-center">
+        Contact Form
+      </h1>
+      <ul>
+        <li>
+          
+        </li>
+      </ul>
+    </div>
+  );
+}
