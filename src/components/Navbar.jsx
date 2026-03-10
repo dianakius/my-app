94826b978ccb535b5ea867a68b67eb22092c71e1
dianakius <eingidiana@gmail.com>
2026-03-10 21:51:32 +0200

    fix:change the color of the two main Links on the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="flex justify-center gap-6 p-4 bg-white shadow">
-      <Link to="/" className="hover:underline">
+      <Link to="/" className="hover:underline text-red-700 ">
         Home
       </Link>
-      <Link to="/cats" className="hover:underline">
+      <Link to="/cats" className="hover:underline text-blue-700">
         Cats
       </Link>
     </nav>
